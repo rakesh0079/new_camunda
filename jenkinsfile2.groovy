df6e8cb3628305a9ee68190a214f4f2d3e46eae3
Rakesh Kumar <35292864+rakesh0079@users.noreply.github.com>
2023-02-05 23:09:58 +0530

    Update jenkinsfile2.groovy

--- jenkinsfile2.groovy
+++ jenkinsfile2.groovy
@@ -1,33 +1,23 @@
 pipeline {
     agent any
     stages {
-stage('Ready for Build'){
-steps{
-echo 'Read for Build'
-}
-
-}
-stage('Build Application'){
-steps{
-    sh 'mvn clean package'
-}
-
-
-post {
-success {
-    echo 'Archive artifact ..'
-    archiveArtifacts artifacts: '**/*.war'
-
-}
-}
-
-
-}
-stage('Create Tomcat Docker Image'){
-steps{
-sh 'docker build . -t camundaTomcat:${env.BUILD_NUMER}'
-}
-
-  }
-  }
+        stage('Build Application') {
+            steps {
+                sh 'mvn -f pom.xml clean package'
+            }
+            post {
+                success {
+                    echo "Now Archiving the Artifacts...."
+                    archiveArtifacts artifacts: '**/*.war'
+                }
+            }
+        }
+ 
+        Stage('Create Tomcat Docker Image'){
+            steps {
+                sh "docker build . -t tomcatsamplewebapp:${env.BUILD_ID}"
+            }
+        }
+ 
+    }
 }
